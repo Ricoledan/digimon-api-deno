4012ac897587cb4305791f01ed7fa46298c30846
Ricardo Ledan <ricoledan@gmail.com>
2020-09-30 12:35:02 -0400

    remove console log

--- drakefile.ts
+++ drakefile.ts
@@ -2,7 +2,6 @@ import { desc, run, task, sh } from 'https://deno.land/x/drake@v1.2.6/mod.ts'
 
 desc('Run Deno Server')
 task('run', [], async function () {
-  console.log('Runnning Application')
   await sh('denon run --allow-net --allow-env --allow-read server.ts')
 })
 
