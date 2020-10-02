74dff2aeb62e3e9afb51bb242f4929fbd147b557
Ricardo Ledan <ricoledan@gmail.com>
2020-10-01 23:59:17 -0400

    add deps pattern, update vscode settings

--- repositories/digimonRepo.ts
+++ repositories/digimonRepo.ts
@@ -1,6 +1,6 @@
-import client from '../database/database.js'
-import { Digimon } from '../types'
-import { moment } from 'https://deno.land/x/moment/moment.ts'
+import client from "../database/database.ts";
+import type { Digimon } from "../types.ts";
+import { moment } from "https://deno.land/x/moment/moment.ts";
 
 class DigimonRepo {
   create(digimon: Digimon) {
@@ -11,20 +11,20 @@ class DigimonRepo {
       digimon.name,
       digimon.description,
       digimon.image_url,
-      moment().format('dddd, MMMM Do YYYY, h:mm:ss a')
-    )
+      moment().format("dddd, MMMM Do YYYY, h:mm:ss a"),
+    );
   }
   selectAll() {
-    return client.query('SELECT * FROM digimon ORDER BY id')
+    return client.query("SELECT * FROM digimon ORDER BY id");
   }
-  selectByName(name) {
-    return client.query(`SELECT * FROM digimon WHERE name == $1`, name)
+  selectByName(name: string) {
+    return client.query(`SELECT * FROM digimon WHERE name == $1`, name);
   }
-  update(name, digimon) {
-    const getDigimon = this.selectByName(name)
+  update(name: string, digimon: Digimon) {
+    const getDigimon = this.selectByName(name);
     const updateQuery = client.query(
-      `UPDATE digimon SET type = $1, level = $2, name = $3, description = $4, image_url = $5 WHERE name = $3`
-    )
+      `UPDATE digimon SET type = $1, level = $2, name = $3, description = $4, image_url = $5 WHERE name = $3`,
+    );
     return client.query(
       updateQuery,
       digimon.type !== undefined ? digimon.type : getDigimon.type,
@@ -33,7 +33,8 @@ class DigimonRepo {
       digimon.description !== undefined
         ? digimon.description
         : getDigimon.description,
-      digimon.image_url !== undefined ? digimon.image_url : getDigimon.image_url
-    )
+      digimon.image_url !== undefined ? digimon.image_url
+      : getDigimon.image_url,
+    );
   }
 }
--- routes.ts
+++ routes.ts
@@ -1,24 +1,24 @@
-import { Router } from 'https://deno.land/x/oak/mod.ts'
+import { Router } from "./deps.ts";
 import {
   getAllDigimon,
   getDigimon,
   addDigimon,
   updateDigimon,
   deleteDigimon,
-} from './controllers/digimon.ts'
+} from "./controllers/digimon.ts";
 
-const router = new Router()
+const router = new Router();
 
 router
-  .get('/', ({ response }: { response: any }) => {
+  .get("/", ({ response }: { response: any }) => {
     response.body = {
-      message: 'YERR!!!',
-    }
+      message: "YERR!!!",
+    };
   })
-  .get('/api/v1/digimon', getAllDigimon)
-  .get('/api/v1/digimon/:name', getDigimon)
-  .post('/api/v1/digimon', addDigimon)
-  .put('/api/v1/digimon/:name', updateDigimon)
-  .delete('/api/v1/digimon/:name', deleteDigimon)
+  .get("/api/v1/digimon", getAllDigimon)
+  .get("/api/v1/digimon/:name", getDigimon)
+  .post("/api/v1/digimon", addDigimon)
+  .put("/api/v1/digimon/:name", updateDigimon)
+  .delete("/api/v1/digimon/:name", deleteDigimon);
 
-export default router
+export default router;
