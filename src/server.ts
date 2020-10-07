89337a5957dca38c50e29c896b3a4b385e3f7436
Ricardo Ledan <ricoledan@gmail.com>
2020-10-07 02:13:01 -0400

    updates

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import { Application, config, parse } from "./deps.ts";
+import { Application, config, flags } from "./deps.ts";
 import router from "./routes.ts";
 
 const env = config();
 const { args } = Deno;
-const port = parse(args).port ? Number(parse(args).port) : 3030;
+const port = flags.parse(args).port ? Number(flags.parse(args).port) : 3030;
 
 const app = new Application();
 
