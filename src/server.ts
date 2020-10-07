df9cfbaa73abbb0b0a697162c486c8b07884e584
Ricardo Ledan <ricoledan@gmail.com>
2020-10-07 01:51:46 -0400

    updates

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import router from "./routes.ts";
 
 const env = config();
 const { args } = Deno;
-const host = "127.0.0.1";
 const port = parse(args).port ? Number(parse(args).port) : 3030;
 
 const app = new Application();
@@ -11,6 +10,6 @@ const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log(`デジタルモンスター ${host}:${port} 🦕`);
+console.log(`デジタルモンスター ${port} 🦕`);
 
 await app.listen({ port });
