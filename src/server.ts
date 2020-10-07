57c2aeecf3b6b97cae388e7ee79c49535355763b
Ricardo Ledan <ricoledan@gmail.com>
2020-10-07 01:46:10 -0400

    update procfile

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,14 @@ import router from "./routes.ts";
 
 const env = config();
 const { args } = Deno;
-const HOST = env.HOST || "127.0.0.1";
-const PORT = parse(args).port ? Number(parse(args).port) : 3030;
+const host = "127.0.0.1";
+const port = parse(args).port ? Number(parse(args).port) : 3030;
 
 const app = new Application();
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log(`デジタルモンスター ${HOST}:${PORT} 🦕`);
+console.log(`デジタルモンスター ${host}:${port} 🦕`);
 
-await app.listen({ PORT });
+await app.listen({ port });
