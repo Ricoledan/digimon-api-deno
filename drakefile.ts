27c5e6ad8a0aeedbf081eeb320a953c30c53f9f1
Ricardo Ledan <ricoledan@gmail.com>
2020-10-08 12:42:21 -0400

    add commands to taskrunner

--- drakefile.ts
+++ drakefile.ts
@@ -1,25 +1,30 @@
-import { desc, run, sh, task } from "https://deno.land/x/drake@v1.2.6/mod.ts";
+import { desc, run, sh, task } from 'https://deno.land/x/drake@v1.2.6/mod.ts'
 
-desc("Run Deno Server");
-task("run", [], async function () {
+desc('Run Deno Server')
+task('run', [], async function () {
   await sh(
-    "deno run --allow-net=:${PORT} --allow-env --allow-read --cached-only src/server.ts --port=${PORT}",
-  );
-});
+    'deno run --allow-net=:${PORT} --allow-env --allow-read --cached-only src/server.ts --port=${PORT}'
+  )
+})
 
-desc("Run Deno Dev Server");
-task("run-dev", [], async function () {
-  await sh("denon run --allow-net --allow-env --allow-read src/server.ts");
-});
+desc('Run Deno Dev Server')
+task('run-dev', [], async function () {
+  await sh('denon run --allow-net --allow-env --allow-read src/server.ts')
+})
 
-// desc("Test Deno Project");
-// task("test", [], async function () {
-//   await sh("");
-// });
+desc('Run Heroku Logs')
+task('heroku-logs', [], async function () {
+  await sh('heroku logs --tail --app digital-monster-api')
+})
 
-desc("Lock Deno Dependencies");
-task("lock", [], async function () {
-  await sh("deno cache --lock=lock.json --lock-write src/deps.ts");
-});
+desc('Test Heroku Procfile')
+task('heroku-local', [], async function () {
+  await sh('heroku local web')
+})
 
-run();
+desc('Lock Deno Dependencies')
+task('lock', [], async function () {
+  await sh('deno cache --lock=lock.json --lock-write src/deps.ts')
+})
+
+run()
