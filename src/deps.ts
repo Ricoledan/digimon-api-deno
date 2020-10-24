cc201324c558c3f2f9be912fa97460f49c9abbad
Ricardo <ricoledan@gmail.com>
2020-10-23 23:06:03 -0400

    add moment library to third party scripts

--- src/deps.ts
+++ src/deps.ts
@@ -5,3 +5,4 @@ export * as log from "https://deno.land/std/log/mod.ts";
 // Third-party dependencies
 export { config } from "https://deno.land/x/dotenv/mod.ts";
 export { Application, Router } from "https://deno.land/x/oak/mod.ts";
+export { moment } from "https://deno.land/x/deno_moment/mod.ts";
