118b0444ff5f1fbe26b6e0e49954f21ddbe1dbc2
Ricardo <ricoledan@gmail.com>
2020-10-25 19:42:49 -0400

    add test response

--- src/controllers/digimon.ts
+++ src/controllers/digimon.ts
@@ -1,9 +1,24 @@
 import { log } from "../deps.ts";
-// import type { Digimon } from '../src/types.ts'
+import type { Digimon } from "../types.ts";
 
-export function getAll() {
+export function getAll(): Digimon[] {
   log.info("GET all digimon");
-  return [{}];
+  return [{
+    type: "reptile",
+    level: "child",
+    name: "Agumon",
+    attribute: "vaccine",
+    profile: null,
+    field: [
+      "metal empire",
+      "virus busters",
+    ],
+    artwork: "https://humulos.com/digimon/images/art/dm20/agu.jpg",
+    abilities: [
+      { name: "attack1", description: "lorem ipsum" },
+      { name: "attack2", description: null },
+    ],
+  }];
 }
 export function getSingle(name: string) {
   log.info(`GET one ${name}`);
