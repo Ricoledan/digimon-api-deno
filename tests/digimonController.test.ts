5b0b2d36573284f63fe407869277a11f56f68736
Ricardo <ricoledan@gmail.com>
2020-10-26 13:36:43 -0400

    add unit folder to further define testing goals

--- tests/digimonController.test.ts
+++
@@ -1,8 +0,0 @@
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import { allDigimon } from "./fixtures/digimonFixtures.ts"
-import { getAll } from "../src/controllers/digimon.ts";
-
-Deno.test("get all digimon returns all digimon with the appropriate schema", () => {
-  const results = getAll();
-  assertEquals(results, allDigimon);
-});
