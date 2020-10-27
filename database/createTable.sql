b0b106f40fbf59e472466de900f3dcdb6dd78d62
Ricardo <ricoledan@gmail.com>
2020-10-26 21:01:56 -0400

    update db schema

--- database/createTable.sql
+++ database/createTable.sql
@@ -1,11 +1,32 @@
+-- create
+DROP TABLE digimon;
 CREATE TABLE IF NOT EXISTS digimon (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     level VARCHAR(10) NOT NULL,
     type VARCHAR(50) NOT NULL,
     attribute VARCHAR(8) NOT NULL,
-    field VARCHAR(20),
-    group VARCHAR(25),
+    field VARCHAR(100),
+    group_theme VARCHAR(25),
+    abilities JSONB,
     profile VARCHAR(255),
-    profile_img VARCHAR(100)
-)
+    profile_img VARCHAR(100),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+-- insert
+INSERT INTO digimon(name, level, type, attribute, field, abilities, profile, profile_img)
+VALUES ('agumon', 'child', 'reptile', 'vaccine', 'Metal Empire, Nature Spirits, Virus Busters, Unknown', '{ "name": "Baby Flame", "description": "Releases a stream of fire from its mouth" }', 'A Reptile Digimon which has grown and become able to walk on two legs', 'https://digimon.net/cimages/digimon/agumon.jpg');
+
+-- update
+UPDATE
+   digimon 
+SET
+   name = ''
+WHERE
+   name = ''
+
+-- delete
+DROP TABLE digimon;
