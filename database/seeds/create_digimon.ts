531822164bc64b7ebf244d2b4e7d6f12b78d05a5
Ricardo <ricoledan@gmail.com>
2020-11-14 20:32:08 -0500

    remove pgsql

--- database/seeds/create_digimon.ts
+++
@@ -1,5 +0,0 @@
-import { Seed } from "https://deno.land/x/nessie/mod.ts";
-
-export const run: Seed = () => {
-  return "INSERT INTO digimon(name, level, type, attribute, field, abilities, profile, profile_img) VALUES ('agumon', 'child', 'reptile', 'vaccine', 'Metal Empire, Nature Spirits, Virus Busters, Unknown', '{ 'name': 'Baby Flame', 'description': 'Releases a stream of fire from its mouth' }', 'A Reptile Digimon which has grown and become able to walk on two legs', 'https://digimon.net/cimages/digimon/agumon.jpg')";
-};
