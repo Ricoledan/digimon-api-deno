e4becac14e8126178df8a8fa1cf0921a0e4a1f7b
Ricardo <ricoledan@gmail.com>
2020-12-06 23:46:17 -0500

    add commmands for docker

--- drakefile.ts
+++ drakefile.ts
@@ -22,6 +22,16 @@ task("lock", [], async function () {
   await sh("deno cache --lock=lock.json --lock-write --unstable src/deps.ts");
 });
 
+desc("Build Docker Image");
+task("dbuild", [], async function () {
+  await sh("docker build -t ricoledan/digimon-api .");
+});
+
+desc("Run Docker Container");
+task("drun", [], async function () {
+  await sh("docker run -it -p 3030:3030 ricoledan/digimon-api");
+});
+
 desc("Run Heroku Logs");
 task("hlogs", [], async function () {
   await sh("heroku logs --tail --app digital-monster-api");
